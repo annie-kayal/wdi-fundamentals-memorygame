f2946d679be5861829e6c358b5a2104ece7ce194
Anamika Kayal <anamikakayal@hotmail.com>
2020-01-15 00:31:20 +0000

    created game logic

--- memroy_game/js/main.js
+++ memroy_game/js/main.js
@@ -1,7 +1,16 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
-console.log("User flipped " + cardThree);
-console.log ("User flipped " + cardTwo);
+
+
+const cards = ["queen","queen","king","king"];
+const cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+if (cardsInPlay[0] === cardsInPlay[2) {
+	alert ("You found a match!");
+}  else {
+	alert ("Sorry try again.");
+}
