10d6c07980e2a79f324d7f047f6236b40b7e9b0f
Anamika Kayal <anamikakayal@hotmail.com>
2020-01-17 14:39:28 +0000

    created game logic

--- memroy_game/js/main.js
+++ memroy_game/js/main.js
@@ -1,8 +1,34 @@
 console.log("Up and running!");
 
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+	rank: "Queen",
+	suit: "Hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "Queen",
+	suit: "Diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "King",
+	suit: "Hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "King",
+	suit: "Diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
+
+console.log("images/queen-of-hearts.png");
+console.log("Hearts");
+console.log("images/king-of-hearts-of-hearts.png");
+console.log("Hearts");
 
 function checkForMath() {
 	if (cardsInPlay[0] === cardsInPlay[2]) {
@@ -13,8 +39,8 @@ alert("Sorry. Try Again.");
 };
 
 function flipCard(cardId) {
-	console.log("user flipped " + cards[cardId]);
-cardsInPlay.push(cardId);
+	console.log("user flipped " + cards[cardId].rank);
+cardsInPlay.push(cardId).rank;
 };
 
 flipCard(0);
